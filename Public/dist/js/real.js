5a20abc5e80ffc97c8fb386449df355bf7caa5cf
Ivan <yifanb@gmail.com>
2017-06-25 21:36:21 +0100

    更新

--- Public/dist/js/real.js
+++ Public/dist/js/real.js
@@ -2,7 +2,6 @@ $(function(){
     $(".loadingsb").show();
     var bid = $.getJSON("../api/bid",function(result){
         $(".zhcon").html("");
-        j = 0;
         $.each(result.LoanInfos, function(i, item){
             $(".zhcon").append("<tr class='list"  + item.ListingId + "' data-amount='" + item.Amount + "' data-months='"+ item.Months +"' data-code='" + item.CreditCode + "'><td>"+ item.ListingId +"</td><td>" + item.Amount + "</td><td>" +item.Months+ "</td><td>"+item.Rate + "%</td><td>" +item.CreditCode+ "</td><td><button type='button' class='btn btn-block btn-success btn-sm' data-toggle='modal' data-lid='" + item.ListingId + "' data-target='#modal-analysis' >分析</button></td><td><button type='button' class='btn btn-block btn-info btn-sm' data-toggle='modal' data-lid='" + item.ListingId + "' data-target='#modal-deal' >投资</button></td></tr>");
         });
@@ -10,7 +9,7 @@ $(function(){
     $.when(bid).done(function(){
         $(".loadingsb").hide();
         $('#example2').DataTable({
-            "paging": true,
+            "paging": false,
             "lengthChange": false,
             "searching": false,
             "ordering": true,
