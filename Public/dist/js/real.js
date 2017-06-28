13c0d08371df77d8832db21be0f026eef2e2b4e7
Ivan <yifanb@gmail.com>
2017-06-28 21:47:06 +0100

    修复投资专区报错bug

--- Public/dist/js/real.js
+++ Public/dist/js/real.js
@@ -12,22 +12,19 @@ $(function(){
         });
     });
 
-    var bid = $.getJSON("../api/bid",function(result){
+    $.getJSON("../api/bid",function(result){
         $(".zhcon").html("");
         $.each(result.LoanInfos, function(i, item){
             var str = "<tr class='list"  + item.ListingId + "' data-amount='" + item.Amount + "' data-months='"+ item.Months +"' data-code='" + item.CreditCode + "'><td>"+ item.ListingId +"</td><td>" + item.Amount + "</td><td>" +item.Months+ "</td><td>"+item.Rate + "%</td><td>" +item.CreditCode+ "</td><td><button type='button' class='btn btn-block btn-success btn-sm' data-toggle='modal' data-lid='" + item.ListingId + "' data-target='#modal-analysis' >分析</button></td><td><button type='button' class='btn btn-block btn-info btn-sm btn-deal' data-toggle='modal' data-lid='" + item.ListingId + "' data-target='#modal-deal' >投资</button></td></tr>";
             $(".zhcon").append(str);
         });
-    });
-
-    $.when(bid).done(function(){
         $(".loadingsb").hide();
         $('#example2').DataTable({
-            "paging": false,
+            "paging": true,
             "lengthChange": false,
             "searching": false,
-            "ordering": true,
-            "info": true,
+            "ordering": false,
+            "info": false,
             "autoWidth": false
         });
     });
