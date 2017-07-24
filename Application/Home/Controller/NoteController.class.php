ffa66089649d9a49b649833c5e2f4ab229068f8b
Ivan <yifanb@gmail.com>
2017-07-24 23:39:59 +0100

    开源数据库

--- Application/Home/Controller/NoteController.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Home\Controller;
-use Think\Controller;
-class NoteController extends Controller {
-    public function index(){
-       $this->display();
-    }
-}
-?>
--- Public/dist/js/header.js
+++
@@ -1,5 +0,0 @@
-$(function () {
-    var localad = location.href.lastIndexOf("/");
-    if(localad>0){addnow = location.href.substring(localad+1,location.href.length);}
-    $(".menu-"+addnow).addClass("active");
-});
