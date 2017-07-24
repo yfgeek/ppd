859fad854ddd7a1a4dd50e1ef34059697722d86b
Ivan <yifanb@gmail.com>
2017-07-24 01:38:06 +0100

    sample

---
+++ Application/Common/Conf/config.sample.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    //数据库配置
+    'DB_TYPE'   =>  'mysql',
+    'DB_HOST'   =>  'localhost',
+    'DB_USER'   =>  'root',
+    // 'DB_PWD'    =>  '',
+    'DB_PWD'    =>  'ppd1234!@#',
+    'DB_NAME'   =>  'ppd',
+    'DB_PREFIX' =>  'tp_',
+);
